ce5c82a18cff7177f73fe093078de78f8f4440aa
Dasha <dashabutenko@list.ru>
2022-12-09 23:24:54 +0400

    add task 3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int n;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
+Console.Write($"\n");
